d5c77459f2f3ed26b3b6d8c5b4b6c215fe6d9d10
DilukshaMadushan <diluksha.diluksha.madushan@gmail.com>
2020-07-19 23:29:33 +0530

    commit 3

--- Dhanuka3/src/Screens/registerScreen.js
+++ Dhanuka3/src/Screens/registerScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {StyleSheet, Text, View, Image, Dimensions,TouchableOpacity,ScrollView,TextInput,AsyncStorage } from 'react-native';
+import {StyleSheet, Text, View, Image, Dimensions,TouchableOpacity,ScrollView,TextInput,AsyncStorage, ActivityIndicator, StatusBar } from 'react-native';
 
 import Images from '../common/Images';
 
@@ -10,6 +10,7 @@ class registerScreen extends Component {
     email:null,
     password:null,
     comfirm_password:null,
+    isLoading:false,
   }
  
 
@@ -37,6 +38,7 @@ class registerScreen extends Component {
   handleSignUp(){
     if((this.state.username!==null)&&(this.state.email!==null)&&(this.state.password!==null)&&(this.state.comfirm_password!==null)){
       if(this.state.password == this.state.comfirm_password){
+          this.setState({isLoading:true});
           fetch('https://www.waytoogo.com/wp-json/wc/v3/customers?consumer_key=ck_62bbbe337d050335cacf5b4ae4ea791c5862125d&consumer_secret=cs_67f41238f54e68ffbd473a3ca6c64c455e735ecd',
           {
             method:'POST',
@@ -48,16 +50,19 @@ class registerScreen extends Component {
               }),
           }).then((response) => response.json())
              .then((responseJson) => {
-
+                
                 // this._storeData('isSigned','true');
                 // this._storeData('profileId',responseJson.id);
-                 console.log(responseJson);
+                 console.log(responseJson.id);
+                 this.setState({isLoading:false});
                  //this._storeData('profilePic',responseJson.avatar_url);
                  //this._storeData('profileName',responseJson.user_name);
                  
                  //global.profileId[0] = responseJson.id;
                  //global.profilePic[0] = responseJson.avatar_url;
                  //global.name[0] = responseJson.user_name;
+
+                 this.props.navigation.navigate('Home');
              })
              .catch((error) => {
                console.error(error);
@@ -74,58 +79,65 @@ class registerScreen extends Component {
   render() {
     const { width } = Dimensions.get('window');
     return (
-      <ScrollView style={styles.container}>
-        <View style={styles.CardView}>
-          <View style={{alignSelf:'center',marginBottom:30}}>
-            <Image source={Images.logo}
-                   style={styles.Logo}>
-            </Image>
-          </View>  
-            <View style={styles.TextView}>
-              <TextInput  style={styles.TextInput}
-                placeholder="Enter User Name"
-                maxLength={40}
-                onChangeText={text => this.setState({username:text})}
-              />
+      <ScrollView>
+          {(this.state.isLoading==false)?
+          <ScrollView style={styles.container}>
+            <View style={styles.CardView}>
+              <View style={{alignSelf:'center',marginBottom:30}}>
+                <Image source={Images.logo}
+                      style={styles.Logo}>
+                </Image>
+              </View>  
+                <View style={styles.TextView}>
+                  <TextInput  style={styles.TextInput}
+                    placeholder="Enter User Name"
+                    maxLength={40}
+                    onChangeText={text => this.setState({username:text})}
+                  />
+                </View>
+                <View style={styles.TextView}>
+                  <TextInput  style={styles.TextInput}
+                    placeholder="Enter Email"
+                    maxLength={40}
+                    onChangeText={text => this.setState({email:text})}
+                  />
+                </View>
+                <View style={styles.TextView}>
+                  <TextInput  style={styles.TextInput}
+                    placeholder="Enter Password"
+                    maxLength={40}
+                    onChangeText={text => this.setState({password:text})}
+                  />
+                </View>
+                <View style={styles.TextView}>
+                  <TextInput  style={styles.TextInput}
+                    placeholder="Comfirm Password"
+                    maxLength={40}
+                    onChangeText={text => this.setState({comfirm_password:text})}
+                  />
+                </View>
+                <TouchableOpacity style={styles.Button}
+                                  activeOpacity={0.5}
+                                  onPress={() => {this.handleSignUp()}}>
+                  <Text style={{color:'#fff',fontWeight:'bold',fontSize:20}}>Sign In</Text>
+                </TouchableOpacity>
+                <View style={{flexDirection:'row',paddingTop:10,alignSelf:'center'}}>
+                  <Text style={{alignSelf:'center'}}>Already a member??</Text>
+                  <TouchableOpacity activeOpacity={0.1}
+                                    onPress={()=>this.props.navigation.navigate('Login')} >
+                      <Text style={{color:'black',
+                                    paddingStart:5,
+                                    fontWeight:'bold',
+                                    fontSize:20}}
+                                    >Login</Text>
+                  </TouchableOpacity>
+                </View>
             </View>
-            <View style={styles.TextView}>
-              <TextInput  style={styles.TextInput}
-                placeholder="Enter Email"
-                maxLength={40}
-                onChangeText={text => this.setState({email:text})}
-              />
-            </View>
-            <View style={styles.TextView}>
-              <TextInput  style={styles.TextInput}
-                placeholder="Enter Password"
-                maxLength={40}
-                onChangeText={text => this.setState({password:text})}
-              />
-            </View>
-            <View style={styles.TextView}>
-              <TextInput  style={styles.TextInput}
-                placeholder="Comfirm Password"
-                maxLength={40}
-                onChangeText={text => this.setState({comfirm_password:text})}
-              />
-            </View>
-            <TouchableOpacity style={styles.Button}
-                              activeOpacity={0.5}
-                              onPress={() => {this.handleSignUp()}}>
-              <Text style={{color:'#fff',fontWeight:'bold',fontSize:20}}>Sign In</Text>
-            </TouchableOpacity>
-            <View style={{flexDirection:'row',paddingTop:10,alignSelf:'center'}}>
-              <Text style={{alignSelf:'center'}}>Already a member??</Text>
-              <TouchableOpacity activeOpacity={0.1}
-                                onPress={()=>this.props.navigation.navigate('Login')} >
-                  <Text style={{color:'black',
-                                paddingStart:5,
-                                fontWeight:'bold',
-                                fontSize:20}}
-                                >Login</Text>
-              </TouchableOpacity>
-            </View>
-        </View>
+          </ScrollView> :
+          <View style={{flex:1,justifyContent:'center',alignItems:'center',marginTop:width*0.7}}>
+            <ActivityIndicator/>
+            <StatusBar barStyle="default"/>
+        </View>}
       </ScrollView>
     );
   }
